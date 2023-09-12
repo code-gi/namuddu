0c6d359ed7f26b0e9b992d155d9f029911de023f
code-gi <143076573+code-gi@users.noreply.github.com>
2023-09-12 06:20:42 -0700

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,46 @@
+#even_numbers
+even_numbers=(2,4,6)
+totaleven_numbers=2+4+6
+print("The sum of even numbers less than 8 is"+str(totaleven_numbers))
+print(totaleven_numbers)
+
+
+#The total number of people
+People=(3,4,5,6,7)
+total_people=3+4+5+6+7
+Average=(total_people)/5
+print(Average)
+
+
+-5+8*6
+c=-5
+d=8
+e=6
+f=d*e
+print(c+f)
+
+
+(55+9)% 9
+g=55
+h=9
+i=9
+j=(g+h)
+print(j%i)
+
+
+20+-3*5/8
+k=20
+l=-3*(5/8)
+print(k+l)
+
+5+15/3*2-8%3
+m=5
+n=15
+p=3
+q=2
+r=-8
+s=3
+t=(5+(15/3)*2)-8
+print(t%s)
+
+
